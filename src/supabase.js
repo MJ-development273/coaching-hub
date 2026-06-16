fff59142cfc5e799772beb30ebc45cd43ec07fac
MJ-development273 <mr.jones69@sky.com>
2026-06-16 22:59:19 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 // 1. Go to https://supabase.com and create a free project
 // 2. Go to Project Settings → API
 // 3. Copy "Project URL" and "anon public" key and paste below
-const SUPABASE_URL = https://bbbadcikbhshxofrefto.supabase.co/rest/v1/
+const SUPABASE_URL = https://bbbadcikbhshxofrefto.supabase.co
 const SUPABASE_ANON_KEY = sb_publishable_Xin-unpJ3PCvTO2Jr-az7A_POxIzYvs
 // ─────────────────────────────────────────────────────────────────────────────
 
