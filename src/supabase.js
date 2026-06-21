1d1e8b797be572b899b67ff7b8a180ab8e6206e7
MJ-development273 <mr.jones69@sky.com>
2026-06-22 00:12:52 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://bbbadcikbhshxofrefto.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_Xin-unpJ3PCvTO2Jr-az7A_POxIzYvs'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiYmFkY2lrYmhzaHhvZnJlZnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzU5MzAsImV4cCI6MjA5NzIxMTkzMH0.HhoPkxopByzJgt9RPHWT4YQV7ZiLHYUg-QrAbGb0E4Q'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
